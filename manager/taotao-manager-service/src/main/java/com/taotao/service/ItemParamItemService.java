aac6b221d4909b6b2a8a86499c8bee870f1bf0c2
HelloWangXiangDong <wang_xiang_dong1@163.com>
2016-08-31 17:57:43 +0800

    goshopping sourceCodes

--- manager/taotao-manager-service/src/main/java/com/taotao/service/ItemParamItemService.java
+++
@@ -1,9 +0,0 @@
-package com.taotao.service;
-
-/**
- * 商品参数详情
- * Created by XDStation on 2016/7/29 0029.
- */
-public interface ItemParamItemService {
-    String getItemParamItem(Long cid);
-}
--- portal/src/main/java/com/taotao/portal/service/ContentService.java
+++
@@ -1,8 +0,0 @@
-package com.taotao.portal.service;
-
-/**
- * Created by XDStation on 2016/8/4 0004.
- */
-public interface ContentService {
-    String getContentList();
-}
